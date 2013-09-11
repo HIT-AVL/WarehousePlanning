42065e0e2cac1c62b63e62eadba712660edcfe0a
limuhit <limuhit@gmail.com>
2013-09-11 16:28:01 +0800

    对原有软件进行修改，添加了创建数据库，设置用户信息的功能

---
+++ 1/conhouse.h
@@ -0,0 +1,22 @@
+#ifndef CONHOUSE_H
+#define CONHOUSE_H
+#include <dbms.h>
+#include <storehouse.h>
+#include <storeshelf.h>
+#include <QVector>
+#include "user.h"
+class conhouse:public DBMS
+{
+private:
+    storehouse get_house_now();
+public:
+    conhouse(userdata hu);
+
+    storehouse house;
+
+    //QVector<storeshelf>;
+    void insert_house(storehouse&);
+    void delete_house();
+};
+
+#endif // CONHOUSE_H
---
+++ 1/mainwindow.h
@@ -0,0 +1,62 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <conhouse.h>
+#include <conbill.h>
+#include <conitem.h>
+#include <conshelf.h>
+#include <storescene.h>
+#include "user.h"
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+protected:
+    storescene * scene;
+    conhouse * house;
+    conshelf * shelf;
+    conitem * item;
+    conbill * bill;
+    userdata lim;
+private slots:
+    void on_massOutputButton_clicked();
+    bool loaduser(userdata &u);
+
+    void on_pathButton_clicked();
+
+    void on_finOutputButton_clicked();
+
+    void on_initHouseButton_clicked();
+
+    void on_initInputButton_clicked();
+
+    void on_finInputButton_clicked();
+
+    void on_massInputButton_clicked();
+
+    void on_initOutputButton_clicked();
+
+    void on_searchButton_clicked();
+
+    void get_items_of_shelf(int);
+    void on_pushButton_3_clicked();
+
+   // void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_4_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
